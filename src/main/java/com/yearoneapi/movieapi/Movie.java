d178641f2af8677a39e83e14e102a3ba7f0c5bb7
jgibson37 <jason.gibson15@yahoo.com>
2020-11-17 13:22:56 -0500

    First Commit

---
+++ src/main/java/com/yearoneapi/movieapi/Movie.java
@@ -0,0 +1,10 @@
+package com.yearoneapi.movieapi;
+
+public class Movie {
+
+    private String title;
+    private String director;
+    private String description;
+    private int thumbsUp;
+    private int thumbsDown;
+}
