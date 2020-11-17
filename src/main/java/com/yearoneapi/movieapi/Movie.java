b5ddc8e90000c95e925dadfd48574979405dc91b
jgibson37 <jason.gibson15@yahoo.com>
2020-11-17 13:25:54 -0500

    building Movie class

--- src/main/java/com/yearoneapi/movieapi/Movie.java
+++ src/main/java/com/yearoneapi/movieapi/Movie.java
@@ -7,4 +7,31 @@ public class Movie {
     private String description;
     private int thumbsUp;
     private int thumbsDown;
+
+    public Movie(String title, String director, String description, int thumbsUp, int thumbsDown) {
+        this.title = title;
+        this.director = director;
+        this.description = description;
+        this.thumbsUp = thumbsUp;
+        this.thumbsDown = thumbsDown;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getThumbsUp() {
+        return thumbsUp;
+    }
+
+    public int getThumbsDown() {
+        return thumbsDown;
+    }
 }
