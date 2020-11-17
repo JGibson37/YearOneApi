70b5647f6cfb8f078c814ae100a006f5661c5441
jgibson37 <jason.gibson15@yahoo.com>
2020-11-17 14:05:33 -0500

    Created populator to make test data created HTTP test file to run http request tests

---
+++ src/main/java/com/yearoneapi/movieapi/Populator.java
@@ -0,0 +1,19 @@
+package com.yearoneapi.movieapi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Populator implements CommandLineRunner {
+    MovieStorage movieStorage;
+
+    public Populator(MovieStorage movieStorage){
+        this.movieStorage = movieStorage;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Movie movie1 = new Movie("Test Movie", "Tester Testing", "Testing the connection", 0, 0);
+        movieStorage.save(movie1);
+    }
+}
