4aeae175a2f2b2c8ced8418b5eeeb721ed8b6f11
jgibson37 <jason.gibson15@yahoo.com>
2020-11-19 13:50:54 -0500

    made local API fetch a function fixed Remote API call to pull JSONs, also made it a callable function

--- FrontEnd/fetchMovies.js
+++ FrontEnd/fetchMovies.js
@@ -1,11 +1,24 @@
 
+const fetchMovieFromLocal = () => {
+fetch("http://localhost:8080/api/movies/")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+}
 
-// const fetchMovieFromApi = async  () => {
-// 	return fetch("http://localhost:8080/api/movie/").then((response) =>
-// 	response.json()
-// 	);
-// }
-
-fetch('http://localhost:8080/api/movies/')
-  .then(response => response.json())
-  .then(data => console.log(data));
+const fetchMovieFromRemote = () => {
+fetch(
+  "https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&page=1&r=json",
+  {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "af02123c16msh99467b35f3f43a0p11645ejsn2373f0b6b10a",
+      "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+    },
+  }
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => {
+    console.error(err);
+  });
+}
